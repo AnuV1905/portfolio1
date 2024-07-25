1dcb45fbada9c263a7d188ac8114d20d90cef11c
Anushka Verma <130084364+AnuV1905@users.noreply.github.com>
2024-07-25 19:14:22 +0000

    unstaged changes for pull

--- src/profile.js
+++ src/profile.js
@@ -26,6 +26,11 @@ const about = {
 // To Add a skill, copy any one below and paste it after the last comma
 const skillsBar = [
   {
+    name: "C++",
+    // To add a custom svg instead of font-awesome icons, add svg path below otherwise just comment it out
+    svg: "public/icons8-c++.svg",
+    //faClass: "fab fa-html5",
+  },{
     name: "HTML5",
     // To add a custom svg instead of font-awesome icons, add svg path below otherwise just comment it out
     svg: "M0 32l34.9 395.8L191.5 480l157.6-52.2L384 32H0zm308.2 127.9H124.4l4.1 49.4h175.6l-13.6 148.4-97.9 27v.3h-1.1l-98.7-27.3-6-75.8h47.7L138 320l53.5 14.5 53.7-14.5 6-62.2H84.3L71.5 112.2h241.1l-4.4 47.7z",
